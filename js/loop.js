2286049467ad9a8f162a0a8a1d33e4bb54cb6bf9
JM-arlopes <jmiguelarlopes@gmail.com>
2023-07-07 18:23:34 -0300

    inserindo loop inicial do site

---
+++ js/loop.js
@@ -0,0 +1,54 @@
+const textDisplay = document.getElementById('text');
+
+const listaFrases = ['Lifor.', 'Forense.', 'Veterinária.'];
+let i = 0; // numeração do item completo de "frases".
+let j = 0; // numeração do caracter do item.
+let fraseMovimentada = [];
+
+let deletando = false;
+let final = false;
+
+const loop = () => {
+
+    if (i < listaFrases.length) {
+
+        const frase = listaFrases[i];
+
+        final = false;
+
+        if (!deletando && j < frase.length) {
+            fraseMovimentada.push(frase[j]);
+            j++
+            textDisplay.innerHTML = fraseMovimentada.join('');
+        }
+
+        if (deletando && j <= frase.length) {
+            fraseMovimentada.pop();
+            j--
+            textDisplay.innerHTML = fraseMovimentada.join('');
+        }
+
+        if (j == frase.length) {
+            final = true;
+            deletando = true;
+        }
+
+        if (deletando && j === 0) {
+            deletando = false;
+            fraseMovimentada = [];
+            i++
+
+            if (i == listaFrases.length) {
+                i = 0
+            }
+        }
+    }
+
+    const speedUp = Math.random() * (80 - 50) + 50;
+    const normalSpeed = Math.random() * (300 - 200) + 200;
+
+    let time = final ? 2000 : deletando ? speedUp : normalSpeed;
+    setTimeout(loop, time);
+}
+
+loop();
